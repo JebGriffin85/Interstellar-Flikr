ae93a1a1210b34cbea0ce6c5f18473460c0ca4a8
jeb griffin <jebgriffin@jebs-MacBook-Pro.local>
2021-03-25 17:30:09 -0500

    started frontend

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,8 @@ const usersRouter = require('./users.js');
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
+const { route } = require('./session.js');
+
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body })
