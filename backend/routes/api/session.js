e18c840878b9fce2147f1d6e2e7860fd69e9f8f5
jeb griffin <jebgriffin@jebs-MacBook-Pro.local>
2021-03-25 15:30:48 -0500

    finished day 1

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
@@ -8,9 +9,24 @@ const router = express.Router();
 
 
 
+
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors,
+];
+
+
+
 // Log in
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
         const { credential, password } = req.body;
 
