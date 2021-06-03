5870967f8d2914e6980e8dfa9f303057fc3a8282
Jeb Griffin <74071724+JebGriffin85@users.noreply.github.com>
2021-06-03 10:07:37 -0500

    discover grid

--- frontend/src/components/DiscoveryPage/index.js
+++ frontend/src/components/DiscoveryPage/index.js
@@ -5,24 +5,29 @@ import { Link } from 'react-router-dom';
 import './DiscoveryPage.css';
 
 function DiscoveryPage () {
-    const photos = useSelector((state) => state.photo.photo)
- 
+    const photos = useSelector((state) => state.photo.photo?.photos)
+    let sliced = photos?.slice(0, 12)
+    
+
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getPhotos())
-    }, [dispatch])
+    }, [dispatch]);
+
     return (
         <div>
         <h4 className='h2'>Click on an image to enlarge or add it to your album</h4>
         <div className='img-container'>
 
-            {photos?.photos.map((photo) =>  
+            {sliced?.map((photo) =>  (
+            <div className='photo-link'>
             <Link to={`/photos/${photo.id}`} key={photo.id}>  
             <img className='images' key={photo.id} src={`https://${photo.photoURL}` } alt="" >
-            </img>  </Link>)}
+            </img>  </Link> </div>))}
+            
         </div>
         </div>
     )
-}
+};
 
 export default DiscoveryPage;
