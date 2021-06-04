0e183a9d00fc7f2e38c4b7d4b9116fa416ff3347
Jeb Griffin <74071724+JebGriffin85@users.noreply.github.com>
2021-06-03 22:14:28 -0500

    dynamic discover pages

--- frontend/src/components/DiscoveryPage/index.js
+++ frontend/src/components/DiscoveryPage/index.js
@@ -1,25 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPhotos } from '../../store/home';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './DiscoveryPage.css';
 
 function DiscoveryPage () {
     const photos = useSelector((state) => state.photo.photo?.photos)
-    let sliced = photos?.slice(0, 12)
-    
+    const [currentPage, setCurrentPage] = useState(photos?.slice(0, 12))
 
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getPhotos())
     }, [dispatch]);
-
+    
     return (
-        <div>
+        <>
+        
+        <div className='outer-container'>
         <h4 className='h2'>Click on an image to enlarge or add it to your album</h4>
+
+        <span onClick={() => setCurrentPage(photos.slice(0, 12))}>1</span>
+        <span onClick={() => setCurrentPage(photos.slice(12, 24))}>2</span>
+        <span onClick={() => setCurrentPage(photos.slice(24, 36))}>3</span>
+        <span onClick={() => setCurrentPage(photos.slice(36, 48))}>4</span>
+        <span onClick={() => setCurrentPage(photos.slice(48, 57))}>5</span>
+
         <div className='img-container'>
 
-            {sliced?.map((photo) =>  (
+            {currentPage?.map((photo) =>  (
             <div className='photo-link'>
             <Link to={`/photos/${photo.id}`} key={photo.id}>  
             <img className='images' key={photo.id} src={`https://${photo.photoURL}` } alt="" >
@@ -27,6 +35,7 @@ function DiscoveryPage () {
             
         </div>
         </div>
+    </>
     )
 };
 
