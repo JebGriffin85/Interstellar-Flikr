413f30f8ab9c7bdf741dbf195244bcb6ae4a65b1
Jeb Griffin <74071724+JebGriffin85@users.noreply.github.com>
2021-06-17 21:33:58 -0500

    local storage now working

--- frontend/src/components/DiscoveryPage/index.js
+++ frontend/src/components/DiscoveryPage/index.js
@@ -2,40 +2,56 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPhotos } from '../../store/home';
 import { Link, NavLink } from 'react-router-dom';
+import useLocalStorage from './useLocalStorage';
 import './DiscoveryPage.css';
 
 function DiscoveryPage () {
     const photos = useSelector((state) => state.photo.photo?.photos)
     const [currentPage, setCurrentPage] = useState(photos?.slice(0, 12))
-    const [pageNumber, setPageNumber] = useState()
+    const [pageNum, setPageNum] = useLocalStorage('page', 1)
 
     const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(getPhotos())
+        if (pageNum === 1) setCurrentPage(photos.slice(0, 12));
+        if (pageNum === 2) setCurrentPage(photos.slice(12, 24));
+        if (pageNum === 3) setCurrentPage(photos.slice(24, 36));
+        if (pageNum === 4) setCurrentPage(photos.slice(48, 57));
+        if (pageNum === 5) setCurrentPage(photos.slice(48, 57));
+        dispatch(getPhotos()) 
     }, [dispatch]);
     
     return (
-        <>                  
+        <>
         
         <div className='outer-container'>
         <h4 className='h2'>Click on an image to enlarge or add it to your album</h4>
-
+        <div className='current-page'>
         <span onClick={() => {
             setCurrentPage(photos.slice(0, 12))
-            setPageNumber(1)
-             localStorage.setItem('page', JSON.stringify(pageNumber))
+            setPageNum(1)
             }}
             >1</span>
         <span onClick={() => {
             setCurrentPage(photos.slice(12, 24))
-            setPageNumber(2)
-            localStorage.setItem('page', JSON.stringify(pageNumber))
+            setPageNum(2)
         }}
             >2</span>
-        <span onClick={() => setCurrentPage(photos.slice(24, 36))}>3</span>
-        <span onClick={() => setCurrentPage(photos.slice(36, 48))}>4</span>
-        <span onClick={() => setCurrentPage(photos.slice(48, 57))}>5</span>
-
+        <span onClick={() => {
+            setCurrentPage(photos.slice(24, 36))
+            setPageNum(3)
+        }}
+            >3</span>
+        <span onClick={() => {
+            setCurrentPage(photos.slice(36, 48))
+            setPageNum(4)
+        }}
+            >4</span>
+        <span onClick={() => {
+            setCurrentPage(photos.slice(48, 57))
+            setPageNum(5)
+        }}
+            >5</span>
+        </div>
         <div className='img-container'>
 
             {currentPage?.map((photo) =>  (
---
+++ frontend/src/components/DiscoveryPage/useLocalStorage.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+function getSavedValue(key, initValue) {
+  const savedValue = JSON.parse(localStorage.getItem(key))
+
+  if (savedValue) return savedValue;
+
+  if (initValue instanceof Function) return initValue();
+
+  return initValue;
+}
+
+export default function useLocalStorage(key, initValue) {
+    const [value, setValue] = useState(() => {
+        return getSavedValue(key, initValue)
+    })
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value))
+    }, [value])
+
+
+    return [value, setValue]
+}
